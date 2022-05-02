8ddd541e9c8aa3a82ccb8756384d8ab36727bad9
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2022-05-02 19:08:55 +0700

    fix: missing dependency

--- src/app/Providers/DependencyServiceProvider.php
+++ src/app/Providers/DependencyServiceProvider.php
@@ -6,17 +6,21 @@ use Illuminate\Support\ServiceProvider;
 use App\Core\Application\Query\DaftarMataKuliah\DaftarMataKuliahQueryInterface;
 use App\Core\Application\Query\DaftarLowongan\DaftarLowonganQueryInterface;
 use App\Core\Application\Query\DaftarLamaran\DaftarLamaranQueryInterface;
+use App\Core\Application\Query\DaftarPelamar\DaftarPelamarQueryInterface;
 use App\Core\Domain\Repository\DosenRepository;
 use App\Core\Domain\Repository\LowonganRepository;
 use App\Core\Domain\Repository\MahasiswaRepository;
 use App\Core\Domain\Repository\AsistenDosenRepository;
+use App\Core\Domain\Repository\MataKuliahRepository;
 use App\Infrastructure\Query\SqlDaftarLowonganQuery;
 use App\Infrastructure\Query\SqlDaftarMataKuliahQuery;
 use App\Infrastructure\Query\SqlDaftarLamaranQuery;
+use App\Infrastructure\Query\SqlDaftarPelamarQuery;
 use App\Infrastructure\Repository\SqlServerDosenRepository;
 use App\Infrastructure\Repository\SqlServerLowonganRepository;
 use App\Infrastructure\Repository\SqlServerMahasiswaRepository;
 use App\Infrastructure\Repository\SqlServerAsistenDosenRepository;
+use App\Infrastructure\Repository\SqlServerMataKuliahRepository;
 
 class DependencyServiceProvider extends ServiceProvider
 {
@@ -26,11 +30,13 @@ class DependencyServiceProvider extends ServiceProvider
         $this->app->bind(DaftarLowonganQueryInterface::class, SqlDaftarLowonganQuery::class);
         $this->app->bind(DaftarMataKuliahQueryInterface::class, SqlDaftarMataKuliahQuery::class);
         $this->app->bind(DaftarLamaranQueryInterface::class, SqlDaftarLamaranQuery::class);
+        $this->app->bind(DaftarPelamarQueryInterface::class, SqlDaftarPelamarQuery::class);
 
         // Repository
         $this->app->bind(LowonganRepository::class, SqlServerLowonganRepository::class);
         $this->app->bind(DosenRepository::class, SqlServerDosenRepository::class);
         $this->app->bind(MahasiswaRepository::class, SqlServerMahasiswaRepository::class);
         $this->app->bind(AsistenDosenRepository::class, SqlServerAsistenDosenRepository::class);
+        $this->app->bind(MataKuliahRepository::class, SqlServerMataKuliahRepository::class);
     }
 }
