b62b506ec96032192a934a4de29ef7cd2af4d15f
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2022-05-02 15:19:24 +0700

    fix: access permission

--- src/resources/views/lowongan/index.blade.php
+++ src/resources/views/lowongan/index.blade.php
@@ -15,14 +15,16 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+    @if (auth()->check() && auth()->user()->nip)
     <a href="{{ route('tambah-lowongan') }}" class="btn btn-primary mt-3">{{ __('Tambah Lowongan') }}</a>
+    @endif
     @foreach($daftar_lowongan as $dl)
         <div class="card mt-3">
             <div class="card-body">
                 <h5 class="card-title">{{ $dl->mata_kuliah_nama }} {{ $dl->kode_kelas }}</h5>
                 <p>{{ __('Gaji') }}: Rp{{ $dl->gaji }}</p>
                 <p>{{ $dl->deskripsi }}</p>
-                @if (auth()->user()->id == $dl->dosen_id)
+                @if (auth()->check() && auth()->user()->id == $dl->dosen_id)
                 <a href="{{ route('ubah-lowongan', ['lowonganId' => $dl->id]) }}" class="btn btn-primary    ">{{ __('Ubah') }}</a>
                 <form method="POST" action="{{ route('hapus-lowongan', ['lowonganId' => $dl->id]) }}">
                     @csrf
@@ -30,7 +32,7 @@
                     <button type="submit" class="btn btn-danger">Delete</button>
                 </form>
                 @endif
-                @if (auth()->user()->nim)
+                @if (auth()->check() && auth()->user()->nim)
                 <form method="POST" action="{{ route('lamar', ['lowonganId' => $dl->id]) }}">
                     @csrf
                     <button type="submit" class="btn btn-success">Lamar</button>
--- src/routes/web.php
+++ src/routes/web.php
@@ -22,19 +22,25 @@ use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::get('/test', [TestController::class, 'test']);
 Route::get('/lowongan', [LowonganController::class, 'index'])->name('lowongan');
-Route::get('/lowongan/tambah-lowongan', [LowonganController::class, 'tambah'])->name('tambah-lowongan');
-Route::post('/lowongan/tambah-lowongan', [LowonganController::class, 'tambahAction']);
-Route::get('/lowongan/ubah-lowongan/{lowonganId:uuid}', [LowonganController::class, 'ubah'])->name('ubah-lowongan');
-Route::post('/lowongan/ubah-lowongan/{lowonganId:uuid}', [LowonganController::class, 'ubahAction']);
-Route::delete('/lowongan/hapus-lowongan/{lowonganId:uuid}', [LowonganController::class, 'deleteAction'])->name('hapus-lowongan');
-Route::post('/lowongan/lamar/{lowonganId:uuid}', [AsistenDosenController::class, 'lamar'])->name('lamar');
-
-Route::get('/daftar', [RegisterController::class, 'daftar']);
-Route::post('/daftar', [RegisterController::class, 'daftarAction']);
-
-Route::get('/masuk', [LoginController::class, 'masuk']);
-Route::post('/masuk', [LoginController::class, 'authenticate']);
-Route::post('/keluar', [LoginController::class, 'logout']);
+Route::middleware('auth')->group(function() {
+    Route::middleware('dosen')->group(function () {
+        Route::get('/lowongan/tambah-lowongan', [LowonganController::class, 'tambah'])->name('tambah-lowongan');
+        Route::post('/lowongan/tambah-lowongan', [LowonganController::class, 'tambahAction']);
+        Route::get('/lowongan/ubah-lowongan/{lowonganId:uuid}', [LowonganController::class, 'ubah'])->name('ubah-lowongan');
+        Route::post('/lowongan/ubah-lowongan/{lowonganId:uuid}', [LowonganController::class, 'ubahAction']);
+        Route::delete('/lowongan/hapus-lowongan/{lowonganId:uuid}', [LowonganController::class, 'deleteAction'])->name('hapus-lowongan');
+    });
+    Route::middleware('mahasiswa')->group(function () {
+        Route::post('/lowongan/lamar/{lowonganId:uuid}', [AsistenDosenController::class, 'lamar'])->name('lamar');
+    });
+    Route::post('/keluar', [LoginController::class, 'logout']);
+});
+Route::middleware('guest')->group(function() {
+    Route::get('/daftar', [RegisterController::class, 'daftar']);
+    Route::post('/daftar', [RegisterController::class, 'daftarAction']);
+    Route::get('/masuk', [LoginController::class, 'masuk']);
+    Route::post('/masuk', [LoginController::class, 'authenticate']);
+});
