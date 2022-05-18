632ec0a8934cd3b6e5bc3c97d9e99a17ddd27fab
Daniel Sugianto <nierurein>
2022-05-18 22:03:42 +0700

    feat: change to 3 column cards view for index lowongan

--- src/resources/views/lowongan/index.blade.php
+++ src/resources/views/lowongan/index.blade.php
@@ -4,13 +4,13 @@
 @section('content')
 <div class="container">
     @if(session('success'))
-        <div class="alert alert-success alert-dismissible fade show d-block" role="alert">
+        <div class="alert alert-success alert-dismissible fade show d-block mt-3" role="alert">
             {{ ucfirst(str_replace('_', ' ', session('success'))) . '.' }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
     @if(session('failed'))
-        <div class="alert alert-danger alert-dismissible fade show d-block" role="alert">
+        <div class="alert alert-danger alert-dismissible fade show d-block mt-3" role="alert">
             {{ ucfirst(str_replace('_', ' ', session('failed'))) . '.' }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
@@ -18,29 +18,34 @@
     @if (auth()->check() && auth()->user()->nip)
     <a href="{{ route('tambah-lowongan') }}" class="btn btn-primary mt-3">{{ __('Tambah Lowongan') }}</a>
     @endif
+
+    <div class="row mt-3">
     @foreach($daftar_lowongan as $dl)
-        <div class="card mt-3">
-            <div class="card-body">
-                <h5 class="card-title">{{ $dl->mata_kuliah_nama }} {{ $dl->kode_kelas }}</h5>
-                <p>{{ __('Gaji') }}: Rp{{ $dl->gaji }}</p>
-                <p>{{ $dl->deskripsi }}</p>
-                <a href="{{ route('detail-lowongan', ['lowonganId' => $dl->id]) }}" class="btn btn-primary    ">{{ __('Lihat Detail') }}</a>
-                @if (auth()->check() && auth()->user()->id == $dl->dosen_id)
-                <a href="{{ route('ubah-lowongan', ['lowonganId' => $dl->id]) }}" class="btn btn-primary    ">{{ __('Ubah') }}</a>
-                <form method="POST" action="{{ route('hapus-lowongan', ['lowonganId' => $dl->id]) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-                @endif
-                @if (auth()->check() && auth()->user()->nim)
-                <form method="POST" action="{{ route('lamar', ['lowonganId' => $dl->id]) }}">
-                    @csrf
-                    <button type="submit" class="btn btn-success">Lamar</button>
-                </form>
-                @endif
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $dl->mata_kuliah_nama }} {{ $dl->kode_kelas }}</h5>
+                    <p>{{ __('Gaji') }}: Rp{{ $dl->gaji }}</p>
+                    <p>{{ $dl->deskripsi }}</p>
+                    <a href="{{ route('detail-lowongan', ['lowonganId' => $dl->id]) }}" class="btn btn-primary    ">{{ __('Lihat Detail') }}</a>
+                    @if (auth()->check() && auth()->user()->id == $dl->dosen_id)
+                    <a href="{{ route('ubah-lowongan', ['lowonganId' => $dl->id]) }}" class="btn btn-primary    ">{{ __('Ubah') }}</a>
+                    <form method="POST" action="{{ route('hapus-lowongan', ['lowonganId' => $dl->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                    @endif
+                    @if (auth()->check() && auth()->user()->nim)
+                    <form method="POST" action="{{ route('lamar', ['lowonganId' => $dl->id]) }}">
+                        @csrf
+                        <button type="submit" class="btn btn-success">Lamar</button>
+                    </form>
+                    @endif
+                </div>
             </div>
         </div>
     @endforeach
+    </div>
 </div>
 @endsection
