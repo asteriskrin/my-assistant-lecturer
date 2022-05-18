b99e20d171d4f6fe134a536f8f7d92cf688fa676
Daniel Sugianto <nierurein>
2022-05-18 22:40:22 +0700

    feat: change to tabular view in index lamaranku page

--- src/resources/views/lamaran/index.blade.php
+++ src/resources/views/lamaran/index.blade.php
@@ -3,6 +3,14 @@
 @section('title', 'Daftar Lamaran')
 @section('content')
 <div class="container">
+    <div class="row mt-3 mx-0">
+        <div class="card">
+            <div class="card-body">
+                <h2>Lamaranku</h2>
+            </div>
+        </div>
+    </div>
+
     @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show d-block" role="alert">
             {{ ucfirst(str_replace('_', ' ', session('success'))) . '.' }}
@@ -15,7 +23,43 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
-    @foreach($daftar_lamaran as $dl)
+
+    <table class="table table-striped mt-3">
+    <thead class="table-dark">
+        <tr>
+            <th scope="col">No. </th>
+            <th scope="col">Mata Kuliah</th>
+            <th scope="col">Kode Kelas</th>
+            <th scope="col">Gaji</th>
+            <th scope="col">Tanggal Lamar</th>
+            <th scope="col">Status Pendaftaran</th>
+            <th scope="col">Status Gaji</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($daftar_lamaran as $dl)
+            <tr>
+                <th scope="row">{{ $loop->index + 1 }}</th>
+                <td>{{ $dl->mata_kuliah_nama }}</td>
+                <td>{{ $dl->kode_kelas }}</td>
+                <td>Rp{{ $dl->gaji }}</td>
+                <td>{{ $dl->lamaran_created_at }}</td>
+                <td>
+                    <span class="badge rounded-pill bg-{{ $dl->lamaran_diterima ? 'success' : 'warning' }}">
+                        {{ $dl->lamaran_diterima ? 'Diterima' : 'Pending' }}
+                    </span>
+                </td>
+                <td>
+                    <span class="badge rounded-pill bg-{{ $dl->lamaran_dibayar ? 'success' : 'danger' }}">
+                        {{ $dl->lamaran_dibayar ? 'Lunas' : 'Belum lunas' }}
+                    </span>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+    </table>
+
+    {{-- @foreach($daftar_lamaran as $dl)
         <div class="card mt-3">
             <div class="card-body">
                 <h5 class="card-title">{{ $dl->mata_kuliah_nama }} {{ $dl->kode_kelas }}</h5>
@@ -26,6 +70,6 @@
                 <p>{{ $dl->deskripsi }}</p>
             </div>
         </div>
-    @endforeach
+    @endforeach --}}
 </div>
 @endsection
