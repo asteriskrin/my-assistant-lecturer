565c0c97f336f9033f76c7187329be33e0fec62b
Daniel Sugianto <nierurein>
2022-05-19 00:42:48 +0700

    feat: tidy update status pelamar form

--- src/resources/views/lamaran/ubah.blade.php
+++ src/resources/views/lamaran/ubah.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.app')
 @section('title', 'Daftar Lamaran')
 @section('content')
-<div class="container">
+<div class="container d-flex flex-column vh-100 justify-content-center align-items-center">
     @if(session('success'))
         <div class="alert alert-success alert-dismissible fade show d-block" role="alert">
             {{ ucfirst(str_replace('_', ' ', session('success'))) . '.' }}
@@ -15,36 +15,42 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
-    <p>Nama: <em>{{ $mahasiswa->getNamaLengkap() }}</em></p>
-    <form method="POST" action="{{ route('ubah-status-pelamar', ['lowonganId' => $asistenDosen->getLowonganId()->id(), 'mahasiswaId' => $mahasiswa->getId()->id()]) }}">
-        @csrf
-        <label>Status Penerimaan</label>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="diterima" id="ubah-diterima" value="1" @if ($asistenDosen->getDiterima()) checked @endif>
-            <label class="form-check-label" for="ubah-diterima">
-                Diterima
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="diterima" id="ubah-tidak-diterima" value="0" @if (!$asistenDosen->getDiterima()) checked @endif>
-            <label class="form-check-label" for="ubah-tidak-diterima">
-                Tidak diterima
-            </label>
-        </div>
-        <label>Status Honor</label>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="dibayar" id="ubah-dibayar" value="1" @if ($asistenDosen->getDibayar()) checked @endif>
-            <label class="form-check-label" for="ubah-dibayar">
-                Sudah dibayar
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="dibayar" id="ubah-belum-dibayar" value="0" @if (!$asistenDosen->getDibayar()) checked @endif>
-            <label class="form-check-label" for="ubah-belum-dibayar">
-                Belum dibayar
-            </label>
-        </div>
-        <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
-    </form>
+
+    <div class="card w-25">
+      <div class="card-body">
+        <h5 class="card-title">Ubah Status Pelamar</h5>
+        <h6 class="card-subtitle mb-3 text-muted">{{ $mahasiswa->getNamaLengkap() }}</h6>
+        <form method="POST" action="{{ route('ubah-status-pelamar', ['lowonganId' => $asistenDosen->getLowonganId()->id(), 'mahasiswaId' => $mahasiswa->getId()->id()]) }}">
+            @csrf
+            <label>Status Penerimaan</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="diterima" id="ubah-diterima" value="1" @if ($asistenDosen->getDiterima()) checked @endif>
+                <label class="form-check-label" for="ubah-diterima">
+                    Diterima
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="diterima" id="ubah-tidak-diterima" value="0" @if (!$asistenDosen->getDiterima()) checked @endif>
+                <label class="form-check-label" for="ubah-tidak-diterima">
+                    Tidak diterima
+                </label>
+            </div>
+            <label class="mt-3">Status Honor</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="dibayar" id="ubah-dibayar" value="1" @if ($asistenDosen->getDibayar()) checked @endif>
+                <label class="form-check-label" for="ubah-dibayar">
+                    Sudah dibayar
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="dibayar" id="ubah-belum-dibayar" value="0" @if (!$asistenDosen->getDibayar()) checked @endif>
+                <label class="form-check-label" for="ubah-belum-dibayar">
+                    Belum dibayar
+                </label>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">{{ __('Save') }}</button>
+        </form>
+      </div>
+    </div>
 </div>
 @endsection
